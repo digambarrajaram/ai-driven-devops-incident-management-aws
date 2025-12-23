1d9c85aaefae03f8ae06a26137fe1003619e3327
digambarrajaram <digambarrajaram2@gmail.com>
2025-12-23 23:32:17 +0530

    added destroy.yml file for manual destruction and updated github OIDC role permission to create Infra resources

--- infra/bootstrap/github-oidc.tf
+++ infra/bootstrap/github-oidc.tf
@@ -50,3 +50,72 @@ resource "aws_iam_role_policy_attachment" "logs" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
+
+############################################
+# Custom policy for Terraform (CI/CD)
+############################################
+
+resource "aws_iam_policy" "terraform_ci_policy" {
+  name        = "github-actions-terraform-autoops"
+  description = "Permissions required by Terraform running in GitHub Actions"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+
+      # ECR (for Terraform + CI)
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:CreateRepository",
+          "ecr:DescribeRepositories",
+          "ecr:PutImageScanningConfiguration",
+          "ecr:TagResource"
+        ]
+        Resource = "*"
+      },
+
+      # IAM (ONLY for autoops roles)
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PassRole"
+        ]
+        Resource = "arn:aws:iam::*:role/autoops-*"
+      },
+
+      # App Runner
+      {
+        Effect = "Allow"
+        Action = [
+          "apprunner:*"
+        ]
+        Resource = "*"
+      },
+
+      # CloudWatch
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:*",
+          "cloudwatch:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+############################################
+# Attach policy to GitHub Actions role
+############################################
+
+resource "aws_iam_role_policy_attachment" "terraform_ci_attach" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = aws_iam_policy.terraform_ci_policy.arn
+}

98d4a6b39bf02eedc7d7e29b2a846c6f76fcb044
digambarrajaram <digambarrajaram2@gmail.com>
2025-12-23 19:45:27 +0530

    updated terraform and deploy.yml code to provision infra at CI/CD runtime instead from local system

--- infra/terraform/apprunner.tf
+++ infra/terraform/apprunner.tf
@@ -7,7 +7,7 @@ resource "aws_apprunner_service" "autoops" {
     }
 
     image_repository {
-      image_identifier      = "${aws_ecr_repository.autoops.repository_url}:latest"
+      image_identifier = "${aws_ecr_repository.autoops.repository_url}:initial"
       image_repository_type = "ECR"
 
       image_configuration {
--- infra/terraform/github-oidc.tf
+++ infra/terraform/github-oidc.tf
@@ -3,17 +3,10 @@
 # GitHub Actions OIDC Provider
 ############################################
 
-resource "aws_iam_openid_connect_provider" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
 }
+
 
 ############################################
 # IAM Role for GitHub Actions CI/CD
@@ -28,7 +21,8 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
